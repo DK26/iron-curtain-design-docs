19532c0557626577da9bd04f4593aadf3541cf7c
David Krasnitsky <dikaveman@gmail.com>
2026-03-12 20:11:19 +0200

    mdbook build

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-56dc9c19.js',
+                path_to_root + 'searchindex-93a3da36.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
